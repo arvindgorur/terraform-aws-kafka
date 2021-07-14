9bbb9c33cf4d1542b76847a25ee5823e6f19c7ca
Gorur <agorur@hoopp.com>
2021-07-14 16:42:05 -0400

    Specify number of broker nodes

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ module "my_msk_cluster" {
     cluster_name = "test-cluster"
     client_subnets = ["subnet-0d197931bccaa49c1", "subnet-0b09f110686a4193d"]
     security_groups = ["sg-019acf359a817f363"]
+    number_of_broker_nodes = 2
     encryption_at_rest_kms_key_arn = "arn:aws:kms:us-east-1:960672390668:key/0dc1b9bb-f664-4750-b971-4aace262ee10"
     environment = "dev"
     tag_application = "my application"
