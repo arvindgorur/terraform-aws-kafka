276c30111a7a60a85c8ef7c635a575255dc3cf0a
Gorur <agorur@hoopp.com>
2021-07-15 13:04:47 -0400

    Remove encryption key

--- kafka/variables.tf
+++ kafka/variables.tf
@@ -35,10 +35,10 @@ variable "security_groups" {
   type        = list(string)
 }
 
-variable "encryption_at_rest_kms_key_arn" {
-  description = "ARN of the KMS key to use to encrypt data at rest"
-  type        = string
-}
+# variable "encryption_at_rest_kms_key_arn" {
+#   description = "ARN of the KMS key to use to encrypt data at rest"
+#   type        = string
+# }
 
 variable "environment" {
   description = "Provide appropriate environment name"
