34f7e758770f89917abc6a6515b0d3f8c4bdb037
Gorur <agorur@hoopp.com>
2021-07-15 13:21:38 -0400

    aaaaaaaaahhhhhhhhhhhh2

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@
 module "my_msk_cluster" {
   source                 = "./kafka"
   cluster_name           = "test-cluster"
-  client_subnets         = ["subnet-0256fd77086e6c0d9", "subnet-0f0aaa99a165384a4"]
+  client_subnets         = ["subnet-0e5ab758f74b76c8a", "subnet-0b86d16bf42854704"]
   security_groups        = ["sg-0b526208abea4c670"]
   number_of_broker_nodes = 2
   #encryption_at_rest_kms_key_arn = "arn:aws:kms:us-east-1:960672390668:key/0dc1b9bb-f664-4750-b971-4aace262ee10"
