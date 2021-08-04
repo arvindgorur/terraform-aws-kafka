45c70a8ec62fbfe704be63afa2b8cc2397b58075
Gorur <agorur@hoopp.com>
2021-08-04 16:55:24 -0400

    Set kafka_versions to list

--- kafka/msk-configuration.tf
+++ kafka/msk-configuration.tf
@@ -1,6 +1,6 @@
 resource "aws_msk_configuration" "configuration" {
   count             = var.server_properties != null ? 1 : 0
-  kafka_versions    = var.kafka_version
+  kafka_versions    = [var.kafka_version]
   name              = var.config_name
   server_properties = var.server_properties
 }
