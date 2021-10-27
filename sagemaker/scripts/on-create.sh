8926502217d38228760c77754d118600d91e6151
Arvind Gorur <agorur@hoopp.com>
2021-10-27 10:29:22 -0400

    Update

--- sagemaker/scripts/on-create.sh
+++ sagemaker/scripts/on-create.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 whoami >> /tmp/on-create.txt
-URL="https://repo.anaconda.com/miniconda/Miniconda3-$VERSION-Linux-x86_64.sh"
+URL="https://repo.anaconda.com/miniconda/Miniconda3-${VERSION}-Linux-x86_64.sh"
 echo $URL >> /tmp/on-create.txt
 sudo -u ec2-user -i <<'EOF'
 unset SUDO_UID
 # Install a separate conda installation via Miniconda
 WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda
 mkdir -p "$WORKING_DIR"
-wget --no-check-certificate https://repo.anaconda.com/miniconda/Miniconda3-$VERSION-Linux-x86_64.sh -O "$WORKING_DIR/miniconda.sh"
+wget --no-check-certificate https://repo.anaconda.com/miniconda/Miniconda3-${VERSION}-Linux-x86_64.sh -O "$WORKING_DIR/miniconda.sh"
 bash "$WORKING_DIR/miniconda.sh" -b -u -p "$WORKING_DIR/miniconda"
 rm -rf "$WORKING_DIR/miniconda.sh"
 # Create a custom conda environment
