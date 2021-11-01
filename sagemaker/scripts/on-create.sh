a6fb6b45d5dfc615966ce57de572d17248833156
Arvind Gorur <agorur@hoopp.com>
2021-11-01 15:31:16 -0400

    update script

--- sagemaker/scripts/on-create.sh
+++ sagemaker/scripts/on-create.sh
@@ -7,7 +7,7 @@ unset SUDO_UID
 KERNEL_NAME="hoopp-custom-conda"
 PYTHON="3.7"
 BIN_PATH=/home/ec2-user/anaconda3/condabin
-$BIN_PATH/conda config --remove channels ocnda-forge
+$BIN_PATH/conda config --remove channels conda-forge
 $BIN_PATH/conda create --yes --name "$KERNEL_NAME" anaconda python="$PYTHON"
 source $BIN_PATH/activate "$KERNEL_NAME"
 pip install --quiet ipykernel
