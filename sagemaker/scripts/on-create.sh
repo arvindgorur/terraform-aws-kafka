6aee197268742739d9dc65bc7f1bc7044d75494b
Arvind Gorur <agorur@hoopp.com>
2021-11-02 09:16:15 -0400

    Update scripts

--- sagemaker/scripts/on-create.sh
+++ sagemaker/scripts/on-create.sh
@@ -5,6 +5,7 @@ yum install cifs-utils unixODBC freetds -y
 cat << "EOF" > /home/ec2-user/create-env.sh
 unset SUDO_UID
 WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda
+mkdir -p "$WORKING_DIR"
 KERNEL_NAME="hoopp-custom-conda"
 PYTHON="3.7"
 wget https://repo.anaconda.com/miniconda/${VERSION} -O "$WORKING_DIR/miniconda.sh"
@@ -15,10 +16,11 @@ source "$WORKING_DIR/miniconda/bin/activate"
 conda config --remove channels conda-forge
 conda create --yes --name "$KERNEL_NAME" anaconda python="$PYTHON"
 conda activate "$KERNEL_NAME"
+#conda config --add channels conda-forge
 pip install --quiet ipykernel
 pip install --quiet boto3
 EOF
 
 chmod +x /home/ec2-user/create-env.sh
 chown ec2-user:ec2-user /home/ec2-user/create-env.sh
-#sudo -u ec2-user nohup /home/ec2-user/create-env.sh >> /home/ec2-user/nohup.out 2>&1 &
+sudo -u ec2-user nohup /home/ec2-user/create-env.sh >> /home/ec2-user/nohup.out 2>&1 &
--- sagemaker/scripts/on-start.sh
+++ sagemaker/scripts/on-start.sh
@@ -2,14 +2,15 @@
 whoami >> /tmp/on-start.txt
 yum install cifs-utils unixODBC freetds -y
 
-cat << "EOF" > /home/ec2-user/on-start.sh
-unset SUDO_UID
-KERNEL_NAME="hoopp-custom-conda"
-BIN_PATH=/home/ec2-user/anaconda3/condabin
-source $BIN_PATH/activate "$KERNEL_NAME"
-python -m ipykernel install --user --name "$KERNEL_NAME" --display-name "Custom ($KERNEL_NAME)"
+sudo -u ec2-user -i <<'EOF'
+WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda/
+source "$WORKING_DIR/miniconda/bin/activate"
+for env in $WORKING_DIR/miniconda/envs/*; do
+    BASENAME=$(basename "$env")
+    source activate "$BASENAME"
+    python -m ipykernel install --user --name "$BASENAME" --display-name "Custom ($BASENAME)"
+done
+# Optionally, uncomment these lines to disable SageMaker-provided Conda functionality.
+# echo "c.EnvironmentKernelSpecManager.use_conda_directly = False" >> /home/ec2-user/.jupyter/jupyter_notebook_config.py
+# rm /home/ec2-user/.condarc
 EOF
-
-chmod +x /home/ec2-user/on-start.sh
-chown ec2-user:ec2-user /home/ec2-user/on-start.sh
-# sudo -u ec2-user nohup /home/ec2-user/on-start.sh >> /home/ec2-user/nohup.out 2>&1 &
